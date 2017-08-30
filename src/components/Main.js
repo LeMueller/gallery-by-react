181a6adb66787f61a11e18ba0ef1e0fe0acb65a3
lezahng <lezhang@techfak.uni-bielefeld.de>
2017-08-30 14:30:49 +0200

    locate the pictiures, but not finish, with bug

--- src/components/Main.js
+++ src/components/Main.js
@@ -39,17 +39,67 @@ imageDatas=(function genImageURL(imageDatasArr){
 		"fileName":"3.png",
 		"title":"questionmark",
 		"desc":"what are you doing?"
-	}
+	},
+	{
+		"fileName":"4.png",
+		"title":"gj",
+		"desc":"good job"
+	},
+
+	{
+		"fileName":"5.png",
+		"title":"smile",
+		"desc":"hahaha"
+	},
+
+	{
+		"fileName":"6.png",
+		"title":"questionmark",
+		"desc":"what are you doing?"
+	},
+	{
+		"fileName":"7.png",
+		"title":"gj",
+		"desc":"good job"
+	},
+
+	{
+		"fileName":"8.png",
+		"title":"smile",
+		"desc":"hahaha"
+	},
+
+	{
+		"fileName":"9.png",
+		"title":"questionmark",
+		"desc":"what are you doing?"
+	},
+
 ]);
 
 //console.log(imageDatas);
 
 //imageDatas=genImageURL(imageDatas);
 
+/*
+*获取区间内的一个随机值
+*/
+function getRangeRandom(low,high){
+	return Math.ceil(Math.random()*(high-low)+low);
+}
+
 class ImgFigure extends React.Component{
 	render(){
+
+		let styleObj = {};
+
+		//如果props属性中制订了这张图片的位置，则使用
+		if(this.props.arrange.pos){
+			styleObj=this.props.arrange.pos;
+		}
+
 		return (
-			<figure className="img-figure">
+			<figure className="img-figure" style={styleObj}>
 				<img src={this.props.data.imageURL}
 					alt={this.props.data.title}/>
 				<figcaption>
@@ -110,30 +160,59 @@ class AppComponent extends React.Component {
 			vPosRangeTopY=vPosRange.topY,
 			vPosRangeX = vPosRange.x,
 
-			imgsArrangeTopArr = [],
+			imgsArrangeTopArr = [], //上册区域图片的状态信息
 
 			topImgNum = Math.ceil(Math.random() * 2), //取一个，或者不取
 
-			topImgSpliceIndex = 0,
+			topImgSpliceIndex = 0, //布局于上部的图片是从数组对象中的哪个拿出来的
 
-			imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
+			imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1); //居中图像的状态信息
 
 			//首先居中centerIdex的图片
 			imgsArrangeCenterArr[0].pos=centerPos;
 
-			//取出要布局上册的图片的状态信息
+			//取出要布局上侧的图片的状态信息
 			topImgSpliceIndex=Math.ceil(Math.random()*(imgsArrangeArr.length - topImgNum));
 			imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
 
 			//布局位于上册的图片
 			imgsArrangeTopArr.forEach(function(value, index){
-				/*
+				
 				imgsArrangeTopArr[index].pos={
-					top:vPosRangeTopY[0] vPosRangeTopY[1],
-					left:
+					top:getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]), 
+					left:getRangeRandom(vPosRangeX[0], vPosRangeX[1])
 				}
-				*/
-			})
+				
+			});
+
+			//布局左右两侧的图片
+			for(let i=0, j=imgsArrangeArr.length, k=j/2; i<j; i++){
+				let hPosRangeLORX=null;
+				//前半部分布局在左边，右半部分布局右边
+				if(i<k){
+					hPosRangeLORX=hPosRangeLeftSecX;
+				}else{
+					hPosRangeLORX=hposRangeRightSecX;
+				}
+
+				imgsArrangeArr[i].pos={
+					top:getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+					left:getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+
+				}
+			}
+
+			//填充回上部区域图片的新信息
+			if(imgsArrangeTopArr && imgsArrangeTopArr[0]){
+				imgsArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr[0]);
+			}
+
+			imgsArrangeArr.splice(centerIndex,0, imgsArrangeCenterArr[0]);
+
+			this.setState({
+				imgsArrangeArr: imgsArrangeArr
+			});
+
 	}
 
 	//组件加载以后，为每张图片计算器位置的范围
@@ -172,7 +251,7 @@ class AppComponent extends React.Component {
 		this.Constant.vPosRange.topY[0] = -halfImgH;
 		this.Constant.vPosRange.topY[1] = halfStageH - halfImgH*3;
 		this.Constant.vPosRange.x[0] = halfStageW-imgW;
-		this.Constant.vPosRange.x[1] = halfImgW;
+		this.Constant.vPosRange.x[1] = halfStageW;
 
 		this.rearrange(0);
 
@@ -194,7 +273,7 @@ class AppComponent extends React.Component {
 			}
 		}
 
-		imgFigures.push(<ImgFigure key={value.fileName.toString()} data={value} ref={'imgFigure'+index}/>);
+		imgFigures.push(<ImgFigure key={value.fileName.toString()} data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]}/>);
 	}.bind(this));
 	
 
